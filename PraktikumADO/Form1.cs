5858caa4d5a9f2fedec47349a4150362931d5250
AfdanAiman <afdanaiman06@gmail.com>
2026-03-13 10:58:42 +0700

    Add project files.

---
+++ PraktikumADO/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace PraktikumADO
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnHitungMhs_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void btnHitungMK_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void txtHasil_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
