b8a738fb96520976686321a5d53ba0c09dfb5426
AfdanAiman <afdanaiman06@gmail.com>
2026-03-13 11:20:40 +0700

    Menampilkan jumlah mahasiswa ke txtHasil

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -64,7 +64,8 @@ namespace PraktikumADO
 
                 int jumlah = (int)cmd.ExecuteScalar();
 
-               
+                txtHasil.Text = jumlah.ToString();
+
             }
             
 
